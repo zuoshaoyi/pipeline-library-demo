271e316bc1d3c8c25c1511ff930a3dfa84dd80d4
zuoshaoyi <zuoshaoyi@fenbi.com>
2021-01-28 17:48:47 +0800

    测试Java

--- src/com/cleverbuilder/JavaClass.groovy
+++ src/com/cleverbuilder/JavaClass.groovy
@@ -5,5 +5,11 @@ public class JavaClass {
     public void javaPrint() {
         System.out.println("Java 输出");
 
+        int res =0;
+        for(int i =0;i<Integer.MAX_VALUE;i++){
+            res+=i;
+        }
+        System.out.println(res);
+        Thread.sleep(30000);
     }
 }
