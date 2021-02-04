c541425ee3694d97ffb45643bab7a2bdf6cceb5e
zuoshaoyi <zuoshaoyi@fenbi.com>
2021-02-04 15:59:33 +0800

    测试

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -14,7 +14,10 @@ def call(String name = 'human') {
 //        def ja = new JavaClass()
 //        ja.javaPrint()
 
-        def instance = Class.forName("com.cleverbuilder.JavaClass").newInstance()
+        def instance1 = this.class.classLoader.rootLoader.loadClass("com.cleverbuilder.JavaClass").newInstance()
+
+
+//        def instance = Class.forName("com.cleverbuilder.JavaClass").newInstance()
 //        instance.javaPrint()
 //        def test = new JavaTest()
 //        test.javaPrint()
